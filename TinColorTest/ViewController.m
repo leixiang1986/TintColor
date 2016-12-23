f0d92e2097c88b959eba18fc4470d542d992f220
leixiang <541150547@qq.com>
2016-12-23 18:25:45 +0800

    提交label，image根据tintcolor变化

--- TinColorTest/ViewController.m
+++ TinColorTest/ViewController.m
@@ -9,6 +9,11 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+@property (weak, nonatomic) IBOutlet UIView *colorView;
+@property (weak, nonatomic) IBOutlet UIButton *btn;
+@property (weak, nonatomic) IBOutlet UILabel *label;
+@property (weak, nonatomic) IBOutlet UIImageView *tinImageView;
+@property (weak, nonatomic) IBOutlet UIView *subView;
 
 @end
 
@@ -17,8 +22,20 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    self.colorView.tintColor = [UIColor yellowColor];
+    self.label.textColor = self.colorView.tintColor;
+    //UIImageRenderingModeAlwaysTemplate图片的颜色会根据tintColor变化
+    UIImage *image = [[UIImage imageNamed:@"train_calendar"] imageWithRenderingMode:(UIImageRenderingModeAlwaysTemplate)];
+    self.tinImageView.image = image;
+    self.tinImageView.contentMode = UIViewContentModeScaleAspectFill;
+    self.subView.backgroundColor = self.colorView.tintColor;
 }
 
+- (IBAction)btnClick:(id)sender {
+    self.colorView.tintColor = [UIColor redColor];
+    self.label.textColor = self.colorView.tintColor;
+    self.subView.backgroundColor = self.colorView.tintColor;
+}
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];

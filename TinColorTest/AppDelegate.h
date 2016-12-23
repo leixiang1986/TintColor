bdd3443dc2b2d0a06eb3111b08db017d4296a637
leixiang <541150547@qq.com>
2016-12-23 17:21:18 +0800

    Initial Commit

---
+++ TinColorTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TinColorTest
+//
+//  Created by 雷祥 on 16/12/23.
+//  Copyright © 2016年 leixiang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TinColorTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TinColorTest
+//
+//  Created by 雷祥 on 16/12/23.
+//  Copyright © 2016年 leixiang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
